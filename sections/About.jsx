5af49ddab2efa9c5758be79c13ed1980c816b5d3
Peav <sokpeav96@gmail.com>
2022-12-12 17:04:15 +0700

    about section

---
+++ sections/About.jsx
@@ -0,0 +1,27 @@
+"use client";
+import { motion } from "framer-motion";
+import { TypingText } from "../components";
+
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
+const About = () => (
+	<section className={`${styles.paddings} relative z-10`}>
+		<div className="gradient-02 z-10" />
+		<motion.div
+			variants={staggerContainer}
+			initial="hidden"
+			whileInView={"show"}
+			viewport={{ once: false, amount: 0.25 }}
+			className={`${styles.innerWidth} mx-auto flex-col ${styles.flexCenter}`}
+		>
+			<TypingText title="| About Metaverse" textStyles="text-center" />
+			<motion.p variants={fadeIn("up", "tween", 0.2, 1)} className="mt-[8px] text-center font-normal sm:text-[32px] text-[20px] text-secondary-white">
+				Metaverse is a new thing in the future, where you can enjoy the virtual world by feeling like it's really real, you can feel what you feel in this metaverse world, because this
+				is really the madness of the metaverse of today, using only VR devices you can easily explore the metaverse world you want, turn your dreams into reality. Let's explore the
+				madness of the metaverse by scrolling down
+			</motion.p>
+		</motion.div>
+	</section>
+);
+
+export default About;
