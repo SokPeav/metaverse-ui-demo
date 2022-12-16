84c7b424198e003a288174cd00cea0690f26831d
Peav <sokpeav96@gmail.com>
2022-12-16 17:19:20 +0700

    new feature section

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -3,8 +3,10 @@ import { motion } from "framer-motion";
 import React, { useState } from "react";
 
 import styles from "../styles";
-import { fadeIn,planetVariants, staggerContainer } from "../utils/motion";
-import { ExploreCard, TitleText, TypingText } from "../components";
+
+import { fadeIn, planetVariants, staggerContainer } from "../utils/motion";
+import { ExploreCard, StartSteps, TitleText, TypingText } from "../components";
+import { startingFeatures } from "../constants";
 const GetStarted = () => (
 	<section className={`${styles.paddings} relative z-10`}>
 		<motion.div
@@ -14,17 +16,18 @@ const GetStarted = () => (
 			viewport={{ once: "false", amount: 0.5 }}
 			className={`${styles.innerWidth} mx-auto flex flex-col lg:flex-row gap-8 `}
 		>
-			<motion.div variants={planetVariants('left')} className={`flex-1 ${styles.flexCenter}`}>
-
-        <img
-        src="/get-started.png"
-        alt="get Start"
-        className="w-[90%] h-[90%] object-contain"
-        />
-        <motion.div>
-          
-        </motion.div>
-      </motion.div>
+			<motion.div variants={planetVariants("left")} className={`flex-1 ${styles.flexCenter}`}>
+				<img src="/get-started.png" alt="get Start" className="w-[90%] h-[90%] object-contain" />
+			</motion.div>
+			<motion.div variants={fadeIn("left", "tween", 0.2, 1)} className="flex-[0.75] flex justify-center flex-col ">
+				<TypingText title="| How Metaversus work" />
+				<TitleText title={<>Get Start With Just a few click</>}></TitleText>
+				<div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px] ">
+					{startingFeatures.map((feature, index) => (
+						<StartSteps key={feature} number={index + 1} text={feature} />
+					))}
+				</div>
+			</motion.div>
 		</motion.div>
 	</section>
 );
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,9 +1,34 @@
-'use client';
+"use client";
 
+import styles from "../styles";
+
+import { fadeIn, planetVariants, staggerContainer } from "../utils/motion";
+import { NewFeatures, ExploreCard, StartSteps, TitleText, TypingText } from "../components";
+import { newFeatures } from "../constants";
+import { motion } from "framer-motion";
 const WhatsNew = () => (
-  <section>
-    What's new section
-  </section>
+	<section className={`${styles.paddings} relative z-10`}>
+		<motion.div
+			variants={staggerContainer}
+			initial="hidden"
+			whileInView={"show"}
+			viewport={{ once: "false", amount: 0.5 }}
+			className={`${styles.innerWidth} mx-auto flex flex-col  lg:flex-row gap-8 `}
+		>
+			<motion.div variants={fadeIn("left", "tween", 0.2, 1)} className="flex-[0.75] flex justify-center flex-col ">
+				<TypingText title="| What's New" />
+				<TitleText title={<>What's new with metaversus</>}></TitleText>
+				<div className="mt-[48px] flex /* flex-wrap */  justify-between gap-[24px] ">
+					{newFeatures.map((feature, index) => (
+						<NewFeatures res key={feature.title} {...feature} />
+					))}
+				</div>
+			</motion.div>
+			<motion.div variants={planetVariants("right")} className={`flex-1 flex-row ${styles.flexCenter}`}>
+				<img src="/whats-new.png" alt="What New" className="w-[90%] h-[90%] object-contain" />
+			</motion.div>
+		</motion.div>
+	</section>
 );
 
 export default WhatsNew;
